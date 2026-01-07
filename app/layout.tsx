adda3d863eda3a085ee52e5dc18293d2f5e0c4e6
pradeep s <pirathims@gmail.com>
2026-01-07 21:23:44 +0530

    icon fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,25 +8,11 @@ const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.app",
+  title: "Zuetech - CAS Maintenance",
+  description: "Created with Zuetech",
+  generator: "Zuetech.com",
   icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
+    icon: "/icon.png",
   },
 }
 
